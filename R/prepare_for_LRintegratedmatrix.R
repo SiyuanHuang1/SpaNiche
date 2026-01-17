1712bf56bed1842e74ecf5365ad762e47196fc63
SiyuanHuang1 <1747261382@qq.com>
2026-01-17 11:03:37 +0800

    0117a

--- R/prepare_for_LRintegratedmatrix.R
+++ R/prepare_for_LRintegratedmatrix.R
@@ -1,14 +1,16 @@
 #' prepare_for_LRintegratedmatrix
 #'
-#' @param seu_obj ST data is processed as a Seurat object with 'Spatial' as the assay name. Seurat object is normalized by using the 'NormalizeData' function.
-#' @param ccc_db This parameter represents the reference for cell-cell interactions, which comes from CellChat.
+#' @description
+#' This function prepares an integrated expression matrix for ligand–receptor analysis in spatial transcriptomics data.
+#' It filters CellChat ligand–receptor interactions based on genes present in the Spatial assay of a Seurat object and returns both the integrated expression matrix and the filtered interaction table.
+#' @param seu_obj A Seurat object containing spatial transcriptomics data, with 'Spatial' as the assay name. The object should be normalized using the NormalizeData function.
+#' @param ccc_db This parameter represents the reference database of cell-cell interactions, e.g., CellChatDB.human, containing interaction, complex, and geneInfo tables.
 #'
-#' @return
+#' @return A list with two elements: (1) a gene/complex-by-spot expression matrix, and (2) a filtered CellChat interaction data frame.
 #' @import tidyverse
 #' @import Seurat
 #' @export
 #'
-#' @examples
 prepare_for_LRintegratedmatrix=function(
     seu_obj,
     ccc_db
@@ -16,41 +18,41 @@ prepare_for_LRintegratedmatrix=function(
   wehave_genes=rownames(seu_obj)
 
 
-  ### 仅基于ccc_db
+  ### Extract reference information from the CellChat database
   cc_interaction=ccc_db$interaction
   cc_complex=ccc_db$complex
   cc_geneInfo=ccc_db$geneInfo
-  # 有多少带complex的受/配体
+  # Identify how many ligands/receptors are complexes
   complex1=setdiff(union(cc_interaction$ligand, cc_interaction$receptor) ,cc_geneInfo$Symbol)
 
 
-  ### 借助自己的数据对上述参考做过滤
+  ### Filter the reference information based on genes present in ST data
   cc_geneInfo=cc_geneInfo%>%dplyr::filter(Symbol %in% wehave_genes)
   tmp_complex=cc_complex%>%apply(2,function(x){x %in% c(wehave_genes,"")})
   cc_complex=cc_complex[!rowSums(tmp_complex) < 4,]
-  # 过滤后有多少候选的complex，以及它的基因矩阵
+  # Number of candidate complexes after filtering and the gene composition matrix of complexes
   complex1=intersect(complex1,rownames(cc_complex))
   cc_complex=cc_complex[complex1,]
 
 
-  ### 提取基因
-  # cc_complex涉及的基因
+  ### Extract genes
+  # Genes involved in complexes
   gene1=unlist(cc_complex)
   gene1=gene1[gene1 != ""]
   gene1=as.character(gene1)
-  # 普通受配体涉及的基因
+  # Genes involved in non-complex ligands and receptors
   gene2=intersect(union(cc_interaction$ligand, cc_interaction$receptor) ,cc_geneInfo$Symbol)
-  # 在基于自己数据的前提下，cc_interaction涉及到的所有基因
+  # All genes involved in CellChat interactions, constrained by the current ST dataset
   gene.used=union(gene1,gene2)
 
 
-  ### 提取数据
-  gene.used=intersect(rownames(seu_obj[["Spatial"]]@data),gene.used) #我的数据包含的基因，调整顺序
-  spatial.data=seu_obj[["Spatial"]]@data[gene.used,] %>% as.data.frame() #我的数据缩小范围
+  ### Extract expression data
+  gene.used=intersect(rownames(seu_obj[["Spatial"]]@data),gene.used) # Genes present in the ST dataset; adjust the order accordingly
+  spatial.data=seu_obj[["Spatial"]]@data[gene.used,] %>% as.data.frame() # Subset the expression matrix to relevant genes
 
 
-  ### 补充complex表达数据
-  new.data=matrix(nrow = length(complex1),ncol = dim(seu_obj)[2]) #complex的表达矩阵
+  ### Add expression data for complexes
+  new.data=matrix(nrow = length(complex1),ncol = dim(seu_obj)[2]) # Expression matrix for complexes
 
   for (i in 1:length(complex1)) {
     one_complex=cc_complex[complex1[i],] %>% as.character()
@@ -66,10 +68,13 @@ prepare_for_LRintegratedmatrix=function(
   new.data=rbind(new.data,spatial.data[gene2,])
 
 
-  ### 过滤原始的互作df
-  cc_interaction=cc_interaction[cc_interaction$ligand %in% rownames(new.data) & cc_interaction$receptor %in% rownames(new.data),]
+  ### Filter the original interaction data frame
+  cc_interaction=cc_interaction[
+    cc_interaction$ligand %in% rownames(new.data) &
+      cc_interaction$receptor %in% rownames(new.data),
+    ]
 
 
-  ### 导出结果
+  ### Export results
   list(new.data,cc_interaction)
 }
