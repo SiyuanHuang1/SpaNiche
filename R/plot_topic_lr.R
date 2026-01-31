41f7c31f0042ed60f2bbb449f6f07a002e1d12dc
SiyuanHuang1 <1747261382@qq.com>
2026-01-31 11:52:28 +0800

    0131

--- R/plot_topic_lr.R
+++ R/plot_topic_lr.R
@@ -1,7 +1,17 @@
-#' plot_topic_lr
+#' Plot topic-specific ligand–receptor (LR) interaction heatmap
 #'
-#' @param topic_lr A dataframe (LR by topic) representing the relationship between topics and LR.
-#' @param topic_lr_quantile_thre A threshold for quantile selection. Only data above this quantile will be considered for visualization. Default is 0.95.
+#' This function visualizes topic-specific ligand–receptor (LR) interactions
+#' as a heatmap. For each topic, LR values are first normalized to
+#' relative contributions, then filtered by a global quantile threshold.
+#' The top LR interactions for each topic are selected and displayed
+#' in separate facets.
+#'
+#' @param topic_lr A numeric data frame or matrix with LR interactions as rows
+#'  and topics as columns. Row names must correspond to LR interaction names.
+#' @param topic_lr_quantile_thre A numeric value between 0 and 1 specifying the
+#'   global quantile threshold used to filter LR values across all topics.
+#'   Only LR–topic pairs with values above this quantile will be considered.
+#'   Default is \code{0.95}.
 #' @param top_num_plot The maximum number of LR interactions to be displayed for each topic in the plot. Default is 10.
 #' @param fill_viridis Specifies the "viridis" color palette option to be used for the heatmap. Default is "F".
 #' @param axis.title.size The font size of the axis titles. Default is 14.
@@ -9,11 +19,9 @@
 #' @param axis.text.y.left.size The font size for the text on the left of the y-axis. Default is 8.
 #' @param strip.text.y.size The font size for the text on the y-axis strip. Default is 12.
 #'
-#' @return
+#' @return A \code{ggplot2} object representing a faceted heatmap of topic-specific LR interactions.
 #' @import tidyverse
 #' @export
-#'
-#' @examples
 plot_topic_lr = function(
     topic_lr,
     topic_lr_quantile_thre = 0.95,
@@ -38,16 +46,19 @@ plot_topic_lr = function(
   topic_lr_small = topic_lr[topic_lr$exp > min_thre,]
   topic_lr_small = topic_lr_small %>% arrange(topic,desc(exp))
   write.csv(topic_lr_small,file = paste0("topic_lr_small_",topic_lr_quantile_thre,".csv"),quote = T,row.names = F)
-  ###相对完整的结果，后续画图只能挑几个画
+  ### Relatively complete results are saved here;
+  ### for visualization, only a subset (top LR per topic) will be plotted.
 
   topic_lr_plot = topic_lr_small %>% group_by(topic) %>% top_n(top_num_plot,wt = exp)
   topic_lr_plot$LRinteraction2 = make.unique(topic_lr_plot$LRinteraction,sep = "_")
   repeat_lr = topic_lr_plot$LRinteraction2[str_detect(topic_lr_plot$LRinteraction2,"_[0-9]{1,}$")]
   repeat_lr2 = str_remove(repeat_lr,"_[0-9]{1,}$")
+
   tmp1 = topic_lr_copy[topic_lr_plot$LRinteraction %>% unique(),]
   tmp2 = topic_lr_copy[repeat_lr2,]
   rownames(tmp2) = repeat_lr
   topic_lr_small = rbind(tmp1,tmp2)
+
   topic_lr_small = topic_lr_small[topic_lr_plot$LRinteraction2,levels(topic_lr_plot$topic)]
   topic_lr_small$LRinteraction2 = rownames(topic_lr_small)
   topic_lr_small = topic_lr_small%>%reshape2::melt(id.vars=c("LRinteraction2"))
